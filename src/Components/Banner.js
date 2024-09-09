a8090f4a015da8415f5024780e3bea32b3404b0a
PONRAJ MURUGAN <161797294+PonrajMurugan@users.noreply.github.com>
2024-09-09 12:52:29 +0530

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -7,7 +7,7 @@ import './Banner.css';
 const Banner = () => {
 
   return (
-    <div className='container margin-top-1 '>
+    <div className=' margin-top-1 '>
      <div id="carouselExampleInterval" class="carousel slide "  data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
